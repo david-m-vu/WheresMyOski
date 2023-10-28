83f0d4ab614e66b130d4f5e30ef9cf8b5f3b8a29
David Vu <david.vu@berkeley.edu>
2023-10-28 05:39:33 -0700

    Frontend setup

---
+++ client/screens/Map/PostTab.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, Image } from "react-native";
+import baseStyles from "../../styles/baseStyles.js";
+
+const PostTab = () => {
+    return (
+        <View style={baseStyles.container}>
+            <StatusBar style="auto" />
+        </View>
+    );
+};
+
+export default PostTab;
