503b71df28e21e4bb86eb694379f41ae1e6d6db5
William Bui <bui.william30@gmail.com>
2023-10-28 06:15:40 -0700

    changed nav bar styling

--- client/App.js
+++ client/App.js
@@ -4,10 +4,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomePage from "./screens/Home/HomePage.js";
 import ProfileScreen from "./screens/Profile/ProfileScreen.js";
+import {Dimensions} from 'react-native';
 
 import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
+const windowHeight = Dimensions.get('window').height;
 
 const App = () => {
     return (
@@ -18,7 +20,8 @@ const App = () => {
                     options={({ navigation, route }) => ({
                         title: "Home",
                         headerStyle: {
-                            backgroundColor: "#f4511e",
+                            backgroundColor: "#21b0ff",
+                            height: windowHeight * 0.15,
                         },
                         headerTintColor: "#fff",
                         headerTitleStyle: {
@@ -47,7 +50,7 @@ const App = () => {
                     options={({ navigation, route }) => ({
                         title: "Profile",
                         headerStyle: {
-                            backgroundColor: "#f4511e",
+                            backgroundColor: "#AEE2FF",
                         },
                         headerTintColor: "#fff",
                         headerTitleStyle: {
--- client/styles/baseStyles.js
+++ client/styles/baseStyles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#000",
+        backgroundColor: "#F0EBE3",
         alignItems: "center",
         justifyContent: "center",
     },
