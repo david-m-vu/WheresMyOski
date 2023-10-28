95e3262d46d5129df930a4f3962ac5146af772d3
David Vu <david.vu@berkeley.edu>
2023-10-28 16:48:23 -0700

    Finished Post, HomePage

--- client/App.js
+++ client/App.js
@@ -4,46 +4,71 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomePage from "./screens/Home/HomePage.js";
 import ProfileScreen from "./screens/Profile/ProfileScreen.js";
-import {Dimensions} from 'react-native';
+import LeaderboardScreen from "./screens/Leaderboard/LeaderboardScreen.js"
+import { Dimensions } from "react-native";
 
 import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+import baseStyles from "./styles/baseStyles.js";
 
 const Stack = createStackNavigator();
-const windowHeight = Dimensions.get('window').height;
+const windowHeight = Dimensions.get("window").height;
 
 const App = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                
                 <Stack.Screen
                     name="Home"
                     options={({ navigation, route }) => ({
-                        title: "Home",
+                        title: "",
                         headerStyle: {
                             backgroundColor: "#21b0ff",
                             height: windowHeight * 0.15,
                         },
-                        headerTintColor: "#fff",
+                        headerTintColor: "#000",
                         headerTitleStyle: {
                             fontWeight: "bold",
                         },
-                        headerRight: () => (
-                            <TouchableOpacity
-                                onPress={() => {
-                                    navigation.navigate("Profile")
-                                }}
-                                style={{ marginRight: 10 }}
-                            >
-                                <Ionicons
-                                    name="person"
-                                    size={24}
-                                    color="black"
-                                />
+                        headerRight: () => {
+                            return (
+                                <View style={{ flexDirection: "row" }}>
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            navigation.navigate("Leaderboard");
+                                        }}
+                                        style={{ marginRight: 10 }}
+                                    >
+                                        <FontAwesome5
+                                            name="trophy"
+                                            size={24}
+                                            color="black"
+                                        />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            navigation.navigate("Profile");
+                                        }}
+                                        style={{ marginRight: 10 }}
+                                    >
+                                        <Ionicons
+                                            name="person"
+                                            size={24}
+                                            color="black"
+                                        />
+                                    </TouchableOpacity>
+                                </View>
+                            );
+                        },
+                        headerLeft: () => (
+                            <TouchableOpacity style={{ marginLeft: 10 }}>
+                                <Text style={baseStyles.headerText}>
+                                    Where's My Oski?
+                                </Text>
                             </TouchableOpacity>
                         ),
                     })}
-                    
                     component={HomePage}
                 />
                 <Stack.Screen
@@ -52,13 +77,29 @@ const App = () => {
                         title: "Profile",
                         headerStyle: {
                             backgroundColor: "#AEE2FF",
+                            height: windowHeight * 0.15,
                         },
-                        headerTintColor: "#fff",
+                        headerTintColor: "#000",
                         headerTitleStyle: {
                             fontWeight: "bold",
                         },
                     })}
                     component={ProfileScreen}
+                />
+                <Stack.Screen
+                    name="Leaderboard"
+                    options={({ navigation, route }) => ({
+                        title: "Leaderboard",
+                        headerStyle: {
+                            backgroundColor: "#AEE2FF",
+                            height: windowHeight * 0.15,
+                        },
+                        headerTintColor: "#000",
+                        headerTitleStyle: {
+                            fontWeight: "bold",
+                        },
+                    })}
+                    component={LeaderboardScreen}
                 />
             </Stack.Navigator>
         </NavigationContainer>
--- client/styles/baseStyles.js
+++ client/styles/baseStyles.js
@@ -5,12 +5,15 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#F0EBE3",
         alignItems: "center",
-        justifyContent: "center",
     },
     text: {
         color: "white",
         fontSize: 20
     },
+    headerText: {
+        fontSize: 20,
+        fontWeight: "bold"
+    }
 });
 
 export default styles;
