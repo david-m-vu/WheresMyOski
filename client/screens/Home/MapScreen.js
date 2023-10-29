70f28fe7a423419114051ad3799296c1fb9fa7ba
David Vu <david.vu@berkeley.edu>
2023-10-29 01:32:10 -0700

    made mapview

--- client/screens/Home/MapScreen.js
+++ client/screens/Home/MapScreen.js
@@ -1,25 +1,42 @@
 import React from "react";
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View, Image } from "react-native";
+import { StyleSheet } from "react-native";
+
 import baseStyles from "../../styles/baseStyles.js";
 
-import MapView from "react-native-maps";
+import MapView, { AnimatedRegion, Animated } from "react-native-maps";
+
+const regionData = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+};
 
 const MapScreen = () => {
     return (
-        <View style={baseStyles.container}>
-            <StatusBar style="auto" />
-            <Text>Map</Text>
-            <MapView
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
-        </View>
+        <MapView
+            style={styles.map}
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}
+        />
     );
 };
+
+const styles = StyleSheet.create({
+    map: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+});
 
 export default MapScreen;
