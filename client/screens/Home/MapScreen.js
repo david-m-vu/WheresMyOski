cb135ee19811e6bed83e8a6e2e0bd9d1a5f44349
David Vu <david.vu@berkeley.edu>
2023-10-29 00:58:52 -0700

    Downloaded map api

--- client/screens/Home/MapScreen.js
+++ client/screens/Home/MapScreen.js
@@ -3,11 +3,21 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View, Image } from "react-native";
 import baseStyles from "../../styles/baseStyles.js";
 
+import MapView from "react-native-maps";
+
 const MapScreen = () => {
     return (
         <View style={baseStyles.container}>
             <StatusBar style="auto" />
             <Text>Map</Text>
+            <MapView
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
         </View>
     );
 };
